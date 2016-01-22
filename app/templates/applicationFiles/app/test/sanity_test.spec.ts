9291d4e53cbd28ea457c3801b6428223e35378e2
Sebastien <seb@dsebastien.net>
2016-01-22 02:55:50 +0100

    Added missing import in tests

--- app/templates/applicationFiles/app/test/sanity_test.spec.ts
+++ app/templates/applicationFiles/app/test/sanity_test.spec.ts
@@ -4,7 +4,7 @@
 // https://github.com/typings/typings/issues/69
 // https://github.com/dsebastien/modernWebDevBuild/issues/76
 
-import {describe, xdescribe, it, xit} from "jasmine";
+import {describe, xdescribe, it, xit, expect} from "jasmine";
 
 describe("sanity checks", () => {
 	it("should be able to test", () => {
